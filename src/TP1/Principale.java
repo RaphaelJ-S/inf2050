47364f39f5b4f37db586c7cabb9ee90186f1b090
unknown <65182431+RaphaelJ-S@users.noreply.github.com>
2020-05-27 10:28:10 -0400

    Ajout dossier Librairies, Modif Principale.java -> identification

--- src/TP1/Principale.java
+++ src/TP1/Principale.java
@@ -3,6 +3,13 @@ package TP1;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+/**
+ * Nom: Raphaël Jacob-Simard
+ * Code Permanant: JACR26038907
+ * Cours: INF2050
+ * Groupe: 20
+ * Professeur: Jacques Berger
+ */
 public class Principale {
     public static final String MSG_ERR_CAST = "Probleme de cast. \n" +
             "Le fichier de sortie n'a pas ete cree.";
@@ -11,13 +18,13 @@ public class Principale {
     public static final String MSG_ERR_FILENOTFOUND = "Ce fichier n'existe pas." +
             "\nLe fichier de sortie n'a pas ete cree.";
     public static final String MSG_ERR_PARAM = "Probleme dans les parametres." +
-            "\nLe fichier de sortie n'a pas ete cree." +
-            " Il doit y avoir exactement 2 parametres: fichier d'entree, " +
+            "\nIl doit y avoir exactement 2 parametres: fichier d'entree, " +
             "fichier sortie." +
             "\nLe fichier de sortie n'a pas ete cree.";
     public static final String MSG_ERR_EXCEPTION = "Probleme general. \n" +
             "Le fichier de sortie n'a pas ete cree.";
     public static final String MSG_FIN_NORMALE = "Fin normale du programme";
+
     public static void main(String[] args) {
 
         try {
@@ -26,9 +33,11 @@ public class Principale {
             }
             String cheminEntree = args[0];
             String cheminSortie = args[1];
+
             Soumission.reponseEligibilite(cheminSortie, Soumission.
                     evalEligibilite(cheminEntree));
             System.out.println(MSG_FIN_NORMALE);
+
         } catch (ParamException pe) {
             System.err.println(MSG_ERR_PARAM);
         } catch (FileNotFoundException fnfe) {
