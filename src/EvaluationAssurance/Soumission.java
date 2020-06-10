7b1072a164f995782c1be7e770c75db953426b8d
unknown <65182431+RaphaelJ-S@users.noreply.github.com>
2020-06-10 10:10:38 -0400

    Modification de la méthode reponseEligibilite pour inclure le montant de l'assurance

--- src/EvaluationAssurance/Soumission.java
+++ src/EvaluationAssurance/Soumission.java
@@ -7,6 +7,7 @@ import net.sf.json.JSONException;
 import net.sf.json.JSONObject;
 import net.sf.json.JSONSerializer;
 import java.io.FileNotFoundException;
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.Period;
 
@@ -24,6 +25,22 @@ public class Soumission {
     public static final int FEMME = 2;
     public static final int SANS_GENRE_OPT1 = 0;
     public static final int SANS_GENRE_OPT2 = 9;
+    public static final String MSG_ERR_CAST = "Probleme de cast. \n" +
+            "Le fichier de sortie n'a pas ete cree.";
+    public static final String MSG_ERR_IO = "Probleme de lecture/ecriture. \n" +
+            "Le fichier de sortie n'a pas ete cree.";
+    public static final String MSG_ERR_FILENOTFOUND = "Ce fichier n'existe pas." +
+            "\nLe fichier de sortie n'a pas ete cree.";
+    public static final String MSG_ERR_PARAM = "Probleme dans les parametres." +
+            "\nIl doit y avoir exactement 2 parametres: fichier d'entree, " +
+            "fichier sortie." +
+            "\nLe fichier de sortie n'a pas ete cree.";
+    public static final String MSG_ERR_EXCEPTION = "Probleme general. \n" +
+            "Le fichier de sortie n'a pas ete cree.";
+    public static final String MSG_ERR_JSON = "Probleme de format JSON. \n"+
+            "Le fichier de sortie na pas ete cree.";
+    public static final String MSG_ERR_DATE = "Probleme dans le format de la" +
+            "date. \nLe fichier de sortie na pas ete cree.";
     //attributs
     private String nom;
     private int genre;
@@ -79,23 +96,28 @@ public class Soumission {
             sports = (JSONArray)root.get("sports");
 
         } catch (FileNotFoundException fnfe) {
-            System.err.println("Erreur");
+            System.err.println(MSG_ERR_FILENOTFOUND);
             System.exit(-1);
         } catch (IOException ioe) {
-            System.err.println("Erreur");
-            System.exit(-1);
-        }catch (ParamException pe) {
-            System.out.println(pe);
-            System.exit(-1);
+            System.err.println(MSG_ERR_IO);
+            System.exit(-2);
+        } catch (ClassCastException cce) {
+            System.err.println(MSG_ERR_CAST);
+            System.exit(-3);
+        } catch (DateTimeException dte) {
+            System.err.println(MSG_ERR_DATE);
+            System.exit(-5);
+        } catch (ParamException pe) {
+            System.err.println(pe.getMessage());
+            System.exit(-4);
         }
     }
     private void validationGenre(int genre) throws ParamException{
-
-        if(genre == HOMME || genre == FEMME || genre ==SANS_GENRE_OPT1 ||
+        if(genre == HOMME || genre == FEMME || genre == SANS_GENRE_OPT1 ||
                 genre == SANS_GENRE_OPT2){
             this.genre = genre;
         }else {
-            throw new ParamException("Genre invalide");
+            throw new ParamException("Genre invalide.");
         }
     }
 
@@ -107,17 +129,22 @@ public class Soumission {
      * @param reponse  La reponse de l'evaluation.
      * @throws IOException  S'il y a erreur dans l'ecriture du fichier.
      */
-    public static void reponseEligibilite(String filePath, boolean reponse)
+    public static void reponseEligibilite(String filePath, boolean reponse,
+                                          CalculAssurance calcul)
             throws IOException, JSONException {
+
         JSONObject jObject = new JSONObject();
         jObject.put("eligible", reponse);
+        if (reponse) {
+            jObject.put("montant_annuel", calcul.getMontantFinal() );
+        }
         DiskFile.saveStringIntoFile(filePath, jObject.toString(1));
     }
-    public int calculAge(String dateDeNaissance) {
-        LocalDate naissance = LocalDate.parse(dateDeNaissance);
-        LocalDate aujourdhui = LocalDate.now();
+    public int calculAge(String dateDeNaissance) throws DateTimeException {
+            LocalDate naissance = LocalDate.parse(dateDeNaissance);
+            LocalDate aujourdhui = LocalDate.now();
 
-        return Period.between(naissance, aujourdhui).getYears();
+            return Period.between(naissance, aujourdhui).getYears();
     }
     /**
      * Evalue l'eligibilite d'une demande d'assurance.
